8cf98e4ec04182449e0b056560f70e294abee140
Habibur Rahman <habibrahman6093@gmail.com>
2018-03-24 21:08:13 +0600

    Update RGB_to_Binary_using_Locally_Adaptive_Thresholding.m

--- RGB_to_Binary_using_Locally_Adaptive_Thresholding.m
+++ RGB_to_Binary_using_Locally_Adaptive_Thresholding.m
@@ -1,7 +1,14 @@
-image=imread('image.jpg');
+image1 = 'rgb2binary1.jpg';
+% image2 = 'rgb2binary2.jpg'
+ 
+image=imread(image1);
 
 first=rgb2gray(image);
 first=imbinarize(first,'adaptive','ForegroundPolarity','dark','Sensitivity',0.2);
 
 imshowpair(first,image,'montage');
+
+
+
+
 
